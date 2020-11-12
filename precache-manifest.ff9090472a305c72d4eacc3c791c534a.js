7dae2dbfbe47c1a324a1a18447b652dd44b65970
Pranith Kumar <pranithkumar79@gmail.com>
2020-11-12 12:15:09 +0530

    Updates

---
+++ precache-manifest.ff9090472a305c72d4eacc3c791c534a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e30a9ae20e6306049da3691f59ff3a1",
+    "url": "/EmployeeList/index.html"
+  },
+  {
+    "revision": "47b393344517b359fabd",
+    "url": "/EmployeeList/static/css/main.2ff9b197.chunk.css"
+  },
+  {
+    "revision": "ab196dc095e21d35a907",
+    "url": "/EmployeeList/static/js/2.d53e9dca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeList/static/js/2.d53e9dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b393344517b359fabd",
+    "url": "/EmployeeList/static/js/main.0f422e9a.chunk.js"
+  },
+  {
+    "revision": "0f6412d6141c8f9e74b1",
+    "url": "/EmployeeList/static/js/runtime-main.72b476ca.js"
+  }
+]);
